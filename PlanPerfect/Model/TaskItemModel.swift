83f7e3081d182b905c3de1e7884397a0c3acbb5b
starryskyyy <vygovskaia.e@gmail.com>
2023-03-25 17:02:05 -0700

    Update TaskItemModel.swift

--- PlanPerfect/Model/TaskItemModel.swift
+++ PlanPerfect/Model/TaskItemModel.swift
@@ -12,4 +12,26 @@ extension TaskItem
     func isCompleted() -> Bool{
         return completeDate != nil
     }
+    
+    func isOverdue() -> Bool{
+        if let due = dueDate {
+                let today = Calendar.current.startOfDay(for: Date())
+                let isPastDueDate = !isCompleted() && (scheduleDate || scheduleTime) && due < today
+                return isPastDueDate && due != today
+            }
+            return false
+    }
+    
+    func overDueColor(color: Color) -> Color{
+        return isOverdue() ? Color(red: 1, green: 0.27, blue: 0.23) : color
+    }
+    
+    func isDueToday() -> Bool{
+        
+        if let due = dueDate{
+            let today = Calendar.current.startOfDay(for: Date())
+            return due == today
+        }
+        return false
+    }
 }
