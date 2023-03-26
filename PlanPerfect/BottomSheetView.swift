2654e859370e089e06528ffc2fa8ec64351a895a
starryskyyy <vygovskaia.e@gmail.com>
2023-03-26 09:11:39 -0700

    Update BottomSheetView.swift

--- PlanPerfect/BottomSheetView.swift
+++ PlanPerfect/BottomSheetView.swift
@@ -19,26 +19,17 @@ struct BottomSheetView: View {
     @State var scheduleTime: Bool
     @State var scheduleDate: Bool
     
-    
-    
-    init(passedTaskItem: TaskItem?, initialDate: Date){
-        if let taskItem = passedTaskItem{
-            _selectedTaskItem = State(initialValue: taskItem)
-            _name = State(initialValue: taskItem.name ?? "")
-            _desc = State(initialValue: taskItem.desc ?? "")
-            _dueDate = State(initialValue: taskItem.dueDate ?? initialDate)
-            _scheduleTime = State(initialValue: taskItem.scheduleTime)
-            _scheduleDate = State(initialValue: taskItem.scheduleDate)
-            
-        }
-        else{
-            _name = State(initialValue: "")
-            _desc = State(initialValue: "")
-            _dueDate = State(initialValue: initialDate)
-            _scheduleTime = State(initialValue:false)
-            _scheduleDate = State(initialValue:false)
-        }
+    init(passedTaskItem: TaskItem?, initialDate: Date) {
+        _selectedTaskItem = State(initialValue: passedTaskItem)
+        _name = State(initialValue: passedTaskItem?.name ?? "")
+        _desc = State(initialValue: passedTaskItem?.desc ?? "")
+        _dueDate = State(initialValue: passedTaskItem?.dueDate ?? initialDate)
+        _scheduleTime = State(initialValue: passedTaskItem?.scheduleTime ?? false)
+        _scheduleDate = State(initialValue: passedTaskItem?.scheduleDate ?? false)
     }
+
+    
+    
     var body: some View {
         NavigationView {
             Form {
@@ -98,16 +89,8 @@ struct BottomSheetView: View {
             .scrollContentBackground(.hidden)
             .frame(maxWidth: .infinity, maxHeight: .infinity)
             .background(Color(#colorLiteral(red: 0.1490196139, green: 0.1490196139, blue: 0.1490196139, alpha: 1)).edgesIgnoringSafeArea(.all))
-            .onAppear {
-                        // assign values of selectedTaskItem to the @State variables
-                        if let taskItem = selectedTaskItem {
-                            name = taskItem.name ?? ""
-                            desc = taskItem.desc ?? ""
-                            dueDate = taskItem.dueDate ?? Date()
-                            scheduleTime = taskItem.scheduleTime
-                            scheduleDate = taskItem.scheduleDate
-                        }
-                    }
+            
+            
         }
     }
 
