bb5c7d0c566f1a5d053e040a159a6c8600e21855
starryskyyy <vygovskaia.e@gmail.com>
2023-03-25 17:02:08 -0700

    Update PlanPerfectApp.swift

--- PlanPerfect/PlanPerfectApp.swift
+++ PlanPerfect/PlanPerfectApp.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-@main
+                                                                                                                                                                                                                                                                                                                                                                                         @main
 struct PlanPerfectApp: App {
     let persistenceController = PersistenceController.shared
 
